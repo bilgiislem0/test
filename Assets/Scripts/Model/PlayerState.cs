6daf2c951cb6470484e20eed836a0f17859b57aa
Mehmet Emin Ünal <131856760+bilgiislem0@users.noreply.github.com>
2026-02-09 23:17:27 +0300

    Create PlayerState.cs file

--- Assets/Scripts/Model/PlayerState.cs
+++ Assets/Scripts/Model/PlayerState.cs
@@ -1,40 +1,33 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace FiftyOnePlus.Model
 {
-    [Serializable]
     public sealed class PlayerState
     {
-        public string PlayerId { get; }
-        public string DisplayName { get; }
+        public PlayerState(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
         public List<Tile> Hand { get; } = new();
         public bool HasOpenedSeries { get; set; }
         public bool HasOpenedPairs { get; set; }
-        public int TotalPenaltyPoints { get; set; }
-
-        public PlayerState(string playerId, string displayName)
-        {
-            PlayerId = playerId;
-            DisplayName = displayName;
-        }
-
-        public int HandValue => Hand.Sum(tile => tile.Value);
 
         public void AddTile(Tile tile)
         {
-            if (tile == null)
-            {
-                throw new ArgumentNullException(nameof(tile));
-            }
-
             Hand.Add(tile);
         }
 
         public bool RemoveTile(Tile tile)
         {
             return Hand.Remove(tile);
+        }
+
+        public int TotalHandValue()
+        {
+            return Hand.Sum(tile => tile.Value);
         }
     }
 }
